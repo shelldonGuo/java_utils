af89815bb2413c1a31691063d5deae547609cbbc
guoxuedong <guoxuedong@xiaomi.com>
2016-06-02 21:04:44 +0800

    update for MIN_VALUE

--- BitSetWrap/BitSetWrap.java
+++ BitSetWrap/BitSetWrap.java
@@ -3,14 +3,14 @@ package com.xiaomi.xmss.dao;
 import java.util.BitSet;
 
 public class BitSetWrap {
-    private static final BitSet bitSet = new BitSet();
-    private static final BitSet negativeBitSet = new BitSet();
-
-    public BitSetWrap() {
-    }
+    private BitSet bitSet = new BitSet();
+    private BitSet negativeBitSet = new BitSet();
+    private int hasMinValue = 0;
 
     public void add(int key) {
-        if (key >= 0) {
+        if (key == Integer.MIN_VALUE) {
+            hasMinValue = 1;
+        } else if (key >= 0) {
             bitSet.set(key);
         } else {
             negativeBitSet.set(-key);
@@ -18,15 +18,19 @@ public class BitSetWrap {
     }
 
     public boolean contains(int key) {
+        if (key == Integer.MIN_VALUE) {
+            return hasMinValue == 1;
+        }
         return (key >= 0) ? bitSet.get(key) : negativeBitSet.get(-key);
     }
 
     public int size() {
-        return bitSet.size() + negativeBitSet.size();
+        return bitSet.size() + negativeBitSet.size() + hasMinValue;
     }
 
     public void clear() {
         bitSet.clear();
         negativeBitSet.clear();
+        hasMinValue = 0;
     }
 }
